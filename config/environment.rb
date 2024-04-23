aa90ec5efaf39a483f10180393b6a590f13f29b3
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2024-04-23 15:27:45 +0300

    env

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
+puts "Loaded SECRET_KEY_BASE: #{ENV['SECRET_KEY_BASE']}"
 
 # Initialize the Rails application.
 Rails.application.initialize!
